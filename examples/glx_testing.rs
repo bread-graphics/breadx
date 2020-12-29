82d39dc053d29fa073ca6e81d1732f96b1cac251
not_a_seagull <notaseagull048@gmail.com>
2020-12-29 11:17:57 -0800

    0.1.11

--- examples/glx_testing.rs
+++ examples/glx_testing.rs
@@ -30,13 +30,13 @@ fn main() -> breadx::Result {
         println!("[{}] {}", i, prop);
     });
 
-    /*let cfg = conn.get_fb_configs_immediate(conn.default_screen_index())?;
+    let cfg = conn.get_fb_configs_immediate(conn.default_screen_index())?;
     println!("Num props per fbc: {}", cfg.num_properties_per_config);
     println!("Num fbcs: {}", cfg.num_configs);
     println!("Est. total size: {}", cfg.num_properties_per_config * cfg.num_configs);
     cfg.properties.iter().enumerate().for_each(|(i, prop)| {
         println!("[{}] {}", i, prop);
-    });*/
+    });
 
     loop {
         let ev = conn.wait_for_event()?;
