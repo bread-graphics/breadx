43603590100dec666333c78967ecf99d48f77f1f
jtnunley <jtnunley01@gmail.com>
2022-06-24 10:29:52 -0700

    unbox wait for event future

--- breadx/src/futures/wait_for_event.rs
+++ breadx/src/futures/wait_for_event.rs
@@ -1,11 +1,10 @@
 // MIT/Apache2 License
 
-use super::TryWithDyn;
+use super::TryWith;
 use crate::{
     display::{AsyncDisplay, AsyncDisplayExt, AsyncStatus},
     Result,
 };
-use alloc::boxed::Box;
 use core::{
     future::Future,
     pin::Pin,
@@ -16,19 +15,15 @@ use x11rb_protocol::protocol::Event;
 
 /// The future returned by the `wait_for_event` function.
 pub struct WaitForEvent<'this, Dpy: ?Sized> {
-    innards: TryWithDyn<'this, Event, Dpy>,
+    innards: TryWith<'this, Event, FnTy, Dpy>,
 }
 
-type FnTy = Box<
-    dyn FnMut(&mut dyn AsyncDisplay, &mut Context<'_>) -> Result<AsyncStatus<Event>>
-        + Send
-        + 'static,
->;
+type FnTy = fn(&mut dyn AsyncDisplay, &mut Context<'_>) -> Result<AsyncStatus<Event>>;
 
 impl<'this, Dpy: AsyncDisplay + ?Sized> WaitForEvent<'this, Dpy> {
     #[allow(clippy::redundant_closure_for_method_calls)]
     pub(crate) fn polling(display: &'this mut Dpy) -> Self {
-        let func: FnTy = Box::new(|display, ctx| display.try_wait_for_event(ctx));
+        let func: FnTy = |display, ctx| display.try_wait_for_event(ctx);
         let try_with = display.try_with(func);
 
         Self { innards: try_with }
